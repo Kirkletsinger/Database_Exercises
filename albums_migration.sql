51bbd1e2fee82241d06f65bfa5618b5ce3ae9309
Steven Letsinger <stevenletsinger@Stevens-MacBook-Pro.local>
2016-09-19 14:31:07 -0500

    added albums_migration exercise

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	artist VARCHAR(100),
+	name VARCHAR(100),
+	release_date INT(4), 
+	sales FLOAT
+	genre VARCHAR(100), 
+	PRIMARY KEY (id) 
+);
