a499238793e9a82fd910549ec1c6dd8b39f774e4
Steven Letsinger <stevenletsinger@Stevens-MacBook-Pro.local>
2016-09-21 15:24:56 -0500

    poo

--- join_test_db.sql
+++ join_test_db.sql
@@ -1,30 +1,73 @@
 use join_test_db;
 
+	--Join Users/Roles
+	SELECT users.name AS user_name, roles.name AS role_name
+	FROM USERS
+	JOIN roles ON users.role_id = roles.id;
+	-- LEFT JOIN
+	SELECT count(role_id), users.name AS user_name, roles.name as role_name
+	FROM users
+	LEFT join roles ON users.role_id = roles.id
+	GROUP BY role_name, user_name
+	ORDER by count(role_id);
+	--RIGHT JOIN
+	SELECT users.name AS user_name, roles.name AS role_name
+	FROM users
+	RIGHT JOIN roles ON users.role_id = roles.id;
 
-CREATE TABLE roles (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
-);
+	---Employees Database
 
-CREATE TABLE users (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  role_id INT UNSIGNED DEFAULT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES roles (id)
-);
+	--1
+		SELECT departments.dept_name, concat(employees.first_name, " ",  employees.last_name) 
+	FROm departments
+	JOIN dept_manager
+	ON departments.dept_no = dept_manager.dept_no
+	JOIN employees
+	ON employees.emp_no = dept_manager.emp_no
+	WHERE to_date > curdate();
 
-INSERT INTO roles (name) VALUES ('admin');
-INSERT INTO roles (name) VALUES ('author');
-INSERT INTO roles (name) VALUES ('reviewer');
-INSERT INTO roles (name) VALUES ('commenter');
+	--Find the name of all departments currently managed by women.
+	SELECT departments.dept_name, concat(employees.first_name, " ", employees.last_name) AS full_name
+	FROM departments
+	JOIN dept_manager
+	ON departments.dept_no = dept_manager.dept_no
+	JOIN employees
+	ON employees.emp_no = dept_manager.emp_no
+	WHERE to_date > curdate() AND employees.gender = 'F';
 
-INSERT INTO users (name, email, role_id) VALUES
-('bob', 'bob@example.com', 1),
-('joe', 'joe@example.com', 2),
-('sally', 'sally@example.com', 3),
-('adam', 'adam@example.com', 3),
-('jane', 'jane@example.com', null),
-('mike', 'mike@example.com', null);
+	--Find the current titles of employees currently working in the Customer Service department.
+
+	SELECT title AS Title, COUNT(title)
+	FROM titles AS t
+	JOIN dept_emp AS de
+	   ON de.emp_no = t.emp_no
+	JOIN departments AS d
+	   ON d.dept_no = de.dept_no
+	WHERE de.to_date = '9999-01-01' AND t.to_date = '9999-01-01' AND d.dept_name = 'Customer Service'
+	GROUP BY title;
+
+	--Find the current salary of all current managers.
+
+
+	SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Current Manager', s.salary AS 'Salary'
+	FROM departments AS d
+	JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
+	JOIN employees AS e ON dm.emp_no = e.emp_no
+	JOIN salaries AS s ON e.emp_no = s.emp_no
+	WHERE dm.to_date = '9999-01-01'
+	AND s.to_date = '9999-01-01';
+
+	--BONUS
+
+	SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name', d.dept_name AS 'Department Name',
+	CONCAT(me.first_name, ' ', me.last_name) AS 'Manager Name'
+	FROM employees AS e
+	JOIN dept_emp AS de
+	ON de.emp_no = e.emp_no
+	JOIN departments AS d
+	ON d.dept_no = de.dept_no
+	JOIN dept_manager AS dm
+	ON dm.dept_no = d.dept_no AND dm.to_date > curdate()
+	JOIN employees AS me
+	ON me.emp_no = dm.emp_no
+	WHERE de.to_date > curdate();
